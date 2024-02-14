91773cf3d27fa099aed7aaaaf10c3d91dcc53732
Emerson <emersonvillalvalabrador@gmail.com>
2024-02-14 10:04:07 +0100

    Introduce el método operativa_cuenta, que englobe las sentencias de la clase Main que operan con el objeto cuenta1.  @Emerson

--- src/G2I/Main.java
+++ src/G2I/Main.java
@@ -13,7 +13,13 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operativa_cuenta();
+    }
+
+	public static void operativa_cuenta() {
+		CCuenta cuenta1;
+		double saldoActual;
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
@@ -28,6 +34,6 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-    }
+	}
     
 }
